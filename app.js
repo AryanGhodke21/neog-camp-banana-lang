0e8eaca8a6fd3dfb626c390f813bbe4668681081
Aryan Ghodke <ghodkearyan@gmail.com>
2021-12-28 13:17:55 +0530

    ..

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var btnTranslate = document.querySelector("btn-translate");
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+
+function clickEventHandler () 
+{
+    console.log("clicked!!");
+    console.log(textInput.value);
+}
+
+btnTranslate.addEventListener("click",clickEventHandler)
+
+
+
